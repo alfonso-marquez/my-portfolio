36ab08a65bef93b714a3511532594187c65c8f64
alfoso-marquez <marquez.josealfonso@gmail.com>
2020-05-20 03:19:38 +0800

    added coming soon on interests tab

--- resources/views/inc/interests.blade.php
+++ resources/views/inc/interests.blade.php
@@ -1,10 +1,7 @@
 <div class="container">
     <div class="interests row mt-3">
         <div class="col-12 pt-3">
-            <h3>Coming Soon!</h3>
-            <h3>Interests</h3>
-            <h3>Music</h3>
-            <h3>Sports</h3>
+            <h5 style="text-align: center">Coming Soon!</h5>
         </div>
     </div>
 </div>
