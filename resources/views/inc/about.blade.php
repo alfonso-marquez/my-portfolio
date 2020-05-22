8ac6f2cd68a43effe6ecae0a5fa2f923d9e77b46
alfoso-marquez <marquez.josealfonso@gmail.com>
2020-05-22 13:42:40 +0800

    Fixed responsiveness

--- resources/views/inc/about.blade.php
+++ resources/views/inc/about.blade.php
@@ -1,5 +1,5 @@
     <div class="row about">
-        <div class="col-10 offset-1 col-sm-10 offset-sm-1 col-lg-4 offset-lg-1 mt-4">
+        <div class="col-12 col-sm-10 offset-sm-1 col-lg-4 offset-lg-1 mt-4">
             <div class="about__overview col-12 mb-5">
                 <h4><strong>Info</strong></h4>
                 <p>Studied at <strong>Polytechnic University of the Philippines</strong></p>
@@ -19,7 +19,7 @@
                 <p>Mobile Number: <strong>09052205985</strong></p>
             </div>
         </div>
-        <div class="col-10 offset-1 col-sm-10 offset-sm-1 col-lg-6 offset-lg-0 mt-4">
+        <div class="col-12 col-sm-10 offset-sm-1 col-lg-6 offset-lg-0 mt-4">
             <div class="about__skills col-12">
             <h4><strong>Skills</strong></h4>
             <canvas id="bar-chart" width="700" height="650"></canvas>
