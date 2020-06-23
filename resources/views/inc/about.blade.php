dfca8ddbd7bea74b3b76eacda09218f331d4272b
alfoso-marquez <marquez.josealfonso@gmail.com>
2020-06-23 18:33:03 +0800

    Update on Skills

--- resources/views/inc/about.blade.php
+++ resources/views/inc/about.blade.php
@@ -22,7 +22,138 @@
         <div class="col-12 col-sm-10 offset-sm-1 col-lg-6 offset-lg-0 mt-4">
             <div class="about__skills col-12">
             <h4><strong>Skills</strong></h4>
-            <canvas id="bar-chart" width="700" height="650"></canvas>
+            {{-- <canvas id="bar-chart" width="700" height="650"></canvas> --}}
+            <div>
+                <div class="row skills-category">
+                    <h5><strong>Frontend</strong></h5>
+                </div>
+                <div class="row">
+                    <div class="skills-content col-12">
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/front/html.png" alt="html logo">
+                            </div>
+                            <p>HTML5</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/front/css.png" alt="css logo">
+                            </div>
+                            <p>CSS3</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/front/js.png" alt="js logo">
+                            </div>
+                            <p>Javascript</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/front/sass.png" alt="sass logo">
+                            </div>
+                            <p>Sass</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/front/vue.png" alt="vue logo">
+                            </div>
+                            <p>Vue</p>
+                        </div>
+                        <div>   
+                            <div class="img-container">
+                                <img src="/image/front/react.png" alt="react logo">
+                            </div>
+                            <p>React</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/front/angular.png" alt="angular logo">
+                            </div>
+                            <p>Angular</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row skills-category">
+                    <h5><strong>Backend</strong></h5>
+                </div>
+                <div class="row">
+                    <div class="skills-content col-12">
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/php.png" alt="php logo">
+                            </div>
+                            <p>PHP</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/laravel.png" alt="laravel logo">
+                            </div>
+                            <p>Laravel</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/node.png" alt="node logo">
+                            </div>
+                            <p>Node</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/java.png" alt="java logo">
+                            </div>
+                            <p>Java</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/c.png" alt="c++ logo">
+                            </div>
+                            <p>C++</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/postgresql.png" alt="postgresql logo">
+                            </div>
+                            <p>PostgreSQL</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/back/mysql.png" alt="mysql logo">
+                            </div>
+                            <p>MySQL</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row skills-category">
+                    <h5><strong>Others</strong></h5>
+                </div>
+                <div class="row">
+                    <div class="skills-content col-10 col-sm-10 col-md-8 col-lg-8">
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/others/git.png" alt="git logo">
+                            </div>
+                            <p>Git</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/others/octobercms.png" alt="octobercms logo">
+                            </div>
+                            <p>OctoberCMS</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/others/photoshop.png" alt="photoshop logo">
+                            </div>
+                            <p>Photoshop</p>
+                        </div>
+                        <div>
+                            <div class="img-container">
+                                <img src="/image/others/premierepro.png" alt="premierepro logo">
+                            </div>
+                            <p>Premiere Pro</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
