4b9b9aaf8391adfff6438f161ce9cc6e24cf17fe
alfonso-marquez <marquez.josealfonso@gmail.com>
2020-08-04 21:44:47 +0800

    hehe

--- resources/views/inc/interests.blade.php
+++ resources/views/inc/interests.blade.php
@@ -1,7 +1,10 @@
 
 <div class="interests row mt-3 ml-2 mr-2">
     <div class="col-12">
-        <example-component></example-component>
+       <div style="text-align-last:center">
+            Happy Monthsary
+       </div>
+        {{-- <example-component></example-component> --}}
         {{-- <div class="form-row mb-2">
         <a href="/sign-in/start" class="btn btn-primary btn-block">Sign in with Github</a>
         </div> --}}
