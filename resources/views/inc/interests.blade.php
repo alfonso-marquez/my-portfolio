0b16aca4d29fb9782c01f741d8f8c787762f59c3
alfonso-marquez <marquez.josealfonso@gmail.com>
2020-09-25 20:41:37 +0800

    some update

--- resources/views/inc/interests.blade.php
+++ resources/views/inc/interests.blade.php
@@ -5,5 +5,6 @@
         {{-- <div class="form-row mb-2">
         <a href="/sign-in/start" class="btn btn-primary btn-block">Sign in with Github</a>
         </div> --}}
+        <h5 class="text-center">Saglit.</h5>
     </div>
 </div>
